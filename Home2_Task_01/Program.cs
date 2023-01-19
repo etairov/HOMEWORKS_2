10b2abf0818e251864d02f99b19a29fb219083a5
e.tairov <ev.tairov@yandex.ru>
2023-01-19 21:50:58 +0700

    Home2_Task_01

--- Home2_Task_01/Program.cs
+++ Home2_Task_01/Program.cs
@@ -3,3 +3,42 @@
 //456 -> 5 782 -> 8 918 -> 1
 //-----------------------------
 
+int EnterYourNumber()
+{
+    Console.WriteLine("Введите любое 3-значное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int PrintSecondNumber(int number)
+{
+      if (number <100 || number > 999)
+      {
+        Console.WriteLine("Число вне диапазона");
+      }
+      else
+      {
+        int result = ((number / 10) % 10);
+        Console.WriteLine($"Введенное число {number}, Вторая цифра {result}.");
+      }
+      return number;
+}
+int number = EnterYourNumber();
+int result = PrintSecondNumber(number);
+
+//-----------------------------------------------------
+//Решение без методов
+
+  /*Console.WriteLine("Введите любое 3-значное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int result = 0;
+
+      if (number <100 || number > 999)
+      {
+        Console.WriteLine("Число вне диапазона");
+      }
+      else
+      {
+        result = ((number / 10) % 10);
+        Console.WriteLine($"Введенное число {number}, Вторая цифра {result}.");
+      }  */
